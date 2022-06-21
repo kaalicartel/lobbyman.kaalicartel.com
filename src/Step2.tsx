fc32471ed2ca074ea8f3aad04dfcba7f313a976a
Gilberto Rodrigues <gilberto.kaali@proton.me>
2022-06-21 21:51:36 +0400

    updated text

--- src/Step2.tsx
+++ src/Step2.tsx
@@ -4,8 +4,12 @@ export default () => {
     <div className="step">
         <h2>Step 2: Relax and accumulate juicy bribes</h2>
         <p className='description'>
-            The lobbyman will now vote for you on your behalf and try to get you the most bribes for your $DDD lock. You
-            can check this page at anytime to claim your accumulated rewards.
+            The lobbyman will now work for you and try to get you the most bribes for your $DDD lock. You
+            can visit this page at anytime to claim your accumulated rewards.
+        </p>
+        <p className='description'>
+            The lobbyman does not sleep. The lobbyman does not eat. His one and only goal in life is to
+            get as much as bribes as possible for you. So sit back and have a martini 🍸
         </p>
     </div>
   );
--- src/StepWithdraw.tsx
+++ src/StepWithdraw.tsx
@@ -5,7 +5,7 @@ export default () => {
         <h2>Withdraw $DDD (Optional)</h2>
         <p className='description'>
           If you feel that you've had enough from the lobbyman, you can withdraw your $DDD tokens
-          from him at any time as long as your lock has expired.
+          from him at any time once as your lock has expired.
         </p>
     </div>
   );
