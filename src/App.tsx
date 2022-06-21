e782004d0018c169620cac553b3b8995e286a3ac
Gilberto Rodrigues <gilberto.kaali@proton.me>
2022-06-21 21:29:16 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Lobbyman</h1>
+        <p className='description'>
+          A simple tool to allow DotDot ($DDD) holders to automate their votes. Giving them the best yields
+          from bribes.
+        </p>
+        <p className='comment'>
+          Created by the Kaali cartel
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
